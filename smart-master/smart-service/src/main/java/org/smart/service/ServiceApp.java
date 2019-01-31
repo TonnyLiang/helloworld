c48b8100d265e7972bd28c4dd3f2529d0e9ccdf3
Liang <Liang@Liang-PC>
2019-01-31 15:18:45 +0800

    add gate authorize

--- smart-master/smart-service/src/main/java/org/smart/service/ServiceApp.java
+++ smart-master/smart-service/src/main/java/org/smart/service/ServiceApp.java
@@ -2,6 +2,7 @@ package org.smart.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -10,11 +11,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+//@EnableCircuitBreaker
 public class ServiceApp 
 {
     public static void main( String[] args )
     {
-//        System.out.println( "Hello World!" );
     	SpringApplication.run(ServiceApp.class, args);
     }
 }
